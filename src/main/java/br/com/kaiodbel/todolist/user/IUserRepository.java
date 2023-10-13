b04f6d97d17295e80701364c4759e926c539d009
Kaio Miranda <kaiorjx@gmail.com>
2023-10-13 00:08:55 -0300

    second

--- src/main/java/br/com/kaiodbel/todolist/user/IUserRepository.java
+++ src/main/java/br/com/kaiodbel/todolist/user/IUserRepository.java
@@ -1,2 +1,9 @@
-package br.com.kaiodbel.todolist.user;public class IUserRepository {
+package br.com.kaiodbel.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
 }
--- src/main/java/br/com/kaiodbel/todolist/user/UserController.java
+++ src/main/java/br/com/kaiodbel/todolist/user/UserController.java
@@ -1,6 +1,10 @@
 package br.com.kaiodbel.todolist.user;
 
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    @Autowired
+    private IUserRepository userRepository;
 
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já Cadastrado!");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }

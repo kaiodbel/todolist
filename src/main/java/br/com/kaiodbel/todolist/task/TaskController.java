d46cfb3720f2bf74f06ef448a694d60aa55cec9e
Kaio Miranda <kaiorjx@gmail.com>
2023-10-18 00:19:24 -0300

    fourth

--- src/main/java/br/com/kaiodbel/todolist/task/TaskController.java
+++ src/main/java/br/com/kaiodbel/todolist/task/TaskController.java
@@ -1,11 +1,15 @@
 package br.com.kaiodbel.todolist.task;
 
+import br.com.kaiodbel.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,9 +19,41 @@ public class TaskController {
     private  ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel created (@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        System.out.println("Chegou no Controller!" + request.getAttribute("idUser"));
+    public ResponseEntity created (@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        // 10-11-2023 - Current
+        // 10-10-2023 - StartAt
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio / data de término deve ser maior do que a data atual!");
+        }
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio deve ser menor do que a data de término!");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    //http://localhot:8080/tasks/88ea5d34-2de8-4281-8d26-d7eb3ded7c30
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return taskRepository.save(task);
     }
 }
